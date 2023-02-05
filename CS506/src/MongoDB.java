afac59a4e1f7524a804399ee26cb8e124bf1c38c
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2023-02-05 14:26:30 -0800

    update

---
+++ CS506/src/MongoDB.java
@@ -0,0 +1,16 @@
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//import org.bson.Document;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//public class MongoDB {
+//
+//    ConnectionString connectionString = new ConnectionString("mongodb+srv://yfeng:<password>@taskcluster.3tpukmi.mongodb.net/?retryWrites=true&w=majority");
+//    MongoClientSettings settings = MongoClientSettings.builder()
+//            .applyConnectionString(connectionString)
+//            .build();
+//    MongoClient mongoClient = MongoClients.create(settings);
+//    MongoDatabase database = mongoClient.getDatabase("test");
+//
+//}
