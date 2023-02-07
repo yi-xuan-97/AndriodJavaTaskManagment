8fe6fc0bf0b4f4aeb2568b1ca123ccff1f15d66d
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2023-02-07 11:59:24 -0800

    update activity

---
+++ TaskApplication/app/src/main/java/edu/pdx/cs506/taskapplication/AddTaskActivity.java
@@ -0,0 +1,16 @@
+package edu.pdx.cs506.taskapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtask);
+
+
+    }
+}
--- TaskApplication/app/src/main/java/edu/pdx/cs506/taskapplication/MainActivity.java
+++ TaskApplication/app/src/main/java/edu/pdx/cs506/taskapplication/MainActivity.java
@@ -2,7 +2,9 @@ package edu.pdx.cs506.taskapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button name = findViewById(R.id.main_signin);
+//        name.setOnClickListener((view -> {
+//            Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+//            startActivity(intent);
+//        }));
     }
 }
